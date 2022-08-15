83c8acf5a7aac5ac41f3fa02187c2562a0829cff
ZLUKADARK <ZLUKA@DESKTOP-Q20QITO>
2022-08-15 21:21:44 +0600

    1st Step to new book

--- Library/ClientApp/src/components/NewBook.tsx
+++ Library/ClientApp/src/components/NewBook.tsx
@@ -5,115 +5,108 @@ import { RouteComponentProps } from 'react-router';
 import { ApplicationState } from '../store';
 import * as Booksstore from '../store/ApiBooks';
 import { Component, ChangeEvent } from "react";
-import ApiBooks from "../store/ApiBooks";
-import IBookData from '../store/ApiBooks';
-import Services from '../Services/BookServices';
 import BookServices from '../Services/BookServices';
 import { METHODS } from 'http';
 import { type } from 'os';
 import { isDate } from 'util';
 import { title } from 'process';
+import IBooksDataCreateUpdate from "../store/ApiBooks"
+import { stat } from 'fs';
 
 type Props = {};
 
-type State = IBookData & {
-    correntBook: Array<IBookData> | null,
-   
-};
-
-
+type State = IBooksDataCreateUpdate;
 
 class NewBook extends React.Component<Props, State> {
     constructor(props: Props) {
         super(props);
+
+        this.onChangeTitle = this.onChangeTitle.bind(this);
+        this.onChangeGenre = this.onChangeGenre.bind(this);
+        this.onChangeReleseDate = this.onChangeReleseDate.bind(this);
+        this.onChangeAuthorId = this.onChangeAuthorId.bind(this);
+        this.saveBook = this.saveBook.bind(this);
+
         this.state = {
-            id: null,
             title: "",
             genre: "",
-            authorId: null,
-            releseDate: new Date,
-            author: {
-                id: null,
-                lName: "",
-                name: "",
-                mName: "",
-            }
-        }
+            releseDate: new Date(),
+            authorId: null
+        };
     }
-    newAuthor = false;
 
-    onChageAuthor = () => {
-        this.newAuthor = true;
-        this.componentDidMount;
-    };
+    onChangeTitle(e: ChangeEvent<HTMLInputElement>) {
+        this.setState({
+            title: e.target.value
+        });
+    }
+    onChangeGenre(e: ChangeEvent<HTMLInputElement>) {
+        this.setState({
+            genre: e.target.value
+        });
+    }
+    onChangeReleseDate(e: ChangeEvent<HTMLInputElement>) {
+        this.setState({
+            releseDate: new Date(e.target.value).toISOString().slice(0, 10)
+        })
+        
+        
+    }
+    onChangeAuthorId(e: ChangeEvent<HTMLInputElement>) {
+        this.setState({
+            authorId: new Number(e.target.value)
+        });
+    }
+    saveBook() {
+        const data: IBooksDataCreateUpdate = {
+            title: this.state.title,
+            genre: this.state.genre,
+            authorId: this.state.authorId,
+            releseDate: this.state.releseDate
+        };
+        BookServices.create(data)
+            .then((response: any) => {
+                this.setState({
+                    id: response.data.id,
+                    title: response.data.title,
+                    genre: response.data.genre,
+                    releseDate: response.data.releseDate,
+                    authorId: response.data.authorId
+                });
+                console.log(response.data);
+            })
+            .catch((e: Error) => {
+                console.log(e);
+            });
+    }
 
     public render() {
+        const { title, genre, authorId, releseDate } = this.state;
         return (
             <>
-                <form>
-                    {(() => {
-                        if (this.newAuthor === true) {
-                                return (
-                                    <div className="row mb-4">
-                                        <div className="col">
-                                            <div className="form-outline">
-                                                <input type="text" id="form6Example1" className="form-control" />
-                                                <label className="form-label" htmlFor="form6Example1">First name</label>
-                                            </div>
-                                        </div>
-                                        <div className="col">
-                                            <div className="form-outline">
-                                                <input type="text" id="form6Example2" className="form-control" />
-                                                <label className="form-label" htmlFor="form6Example2">Last name</label>
-                                            </div>
-                                        </div>
-                                        <div className="col">
-                                            <div className="form-outline">
-                                                <input type="text" id="form6Example2" className="form-control" />
-                                                <label className="form-label" htmlFor="form6Example2">Middle name</label>
-                                            </div>
-                                        </div>
-                                    </div>
-                                )
-                            }
-                        })()}
-                    <div className="form-outline mb-4">
-                        <input type="text" id="form6Example3" className="form-control" />
-                        <label className="form-label" htmlFor="form6Example3">Book title</label>
-                    </div>
+                <div className="form-outline mb-4">
+                    <input value={title} onChange={this.onChangeTitle} name="title" type="text" className="form-control" />
+                    <label className="form-label" htmlFor="form6Example3">Book title</label>
+                </div>
 
-     
-                    <div className="form-outline mb-4">
-                        <input type="text" id="form6Example4" className="form-control" />
-                        <label className="form-label" htmlFor="form6Example4">Genre</label>
-                    </div>
+                <div className="form-outline mb-4">
+                    <input  value={genre} onChange={this.onChangeGenre} name="genre" type="text" className="form-control" />
+                    <label className="form-label" htmlFor="form6Example4">Genre</label>
+                </div>
 
-    
-                    <div className="form-outline mb-4">
-                        <input type="email" id="form6Example5" className="form-control" />
-                        <label className="form-label" htmlFor="form6Example5">Email</label>
-                    </div>
+                <div className="form-outline mb-4">
+                    <input value={authorId} onChange={this.onChangeAuthorId} name="authorId" type="number" className="form-control" />
+                    <label className="form-label" htmlFor="form6Example5">Author Id</label>
+                </div>
 
-        
-                    <div className="form-outline mb-4">
-                        <input type="number" id="form6Example6" className="form-control" />
-                        <label className="form-label" htmlFor="form6Example6">Phone</label>
-                    </div>
-
-         
-                    <div className="form-outline mb-4">
-                        <textarea className="form-control" id="form6Example7"></textarea>
-                        <label className="form-label" htmlFor="form6Example7">Additional information</label>
-                    </div>
-
-               
-                    <div className="form-check d-flex justify-content-center mb-4">
-                        <input className="form-check-input me-2" type="checkbox" value=""  id="form6Example8" />
-                    </div>
+                <div className="form-outline mb-4">
+                    <input value={releseDate} onChange={this.onChangeReleseDate} name="releseDate" type="date" className="form-control" />
+                    <label className="form-label" htmlFor="form6Example5">Relese date</label>
+                    
+                </div>
                     
              
-                    <button type="submit" className="btn btn-primary btn-block mb-4" onClick={this.newAuthor === true}>Place order</button>
-                </form>
+                <button type="submit" className="btn btn-primary btn-block mb-4" onClick={this.saveBook}>Place order</button>
             </>
         );
     }
